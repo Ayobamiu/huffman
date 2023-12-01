313856a4ab727cc66ad0d205f31cd7823aaed62f
Ayobamiu <ayobamiu@gmail.com>
2023-11-30 22:56:57 -0500

    Updated

--- huffman_code.cpp
+++ huffman_code.cpp
@@ -1,6 +1,7 @@
 // huffman_practice.cpp: Usman Ayobami
 // Description:  Huffman Coding in C++
 // Reference Code: https://www.programiz.com/dsa/huffman-coding#:~:text=Huffman%20Coding%20Complexity,is%20O(nlog%20n)%20.
+// Compile with "g++ -std=c++11 -o huff huffman_code.cpp"
 
 /*
 Huffman Coding Algorithm
@@ -13,18 +14,17 @@ for all the unique characters:
     insert this newNode into the tree
 return rootNode
 */
-
 #include <iostream>
 #include <iomanip>
 #include <algorithm>
 #include <vector>
 #include "huffman_data.h"
+#include <stdlib.h>
 using namespace std;
 
 #define k_max_tree_height 26
 #define k_table_data_width 15
 #define letters_size 26
-
 
 /* Prints out the result in a table format. */
 template <typename T>
@@ -34,9 +34,9 @@ void FormatDataInTabularForm(T t)
 }
 
 // Creating Heap node
-HeapNode* CreateHeapNode(char item, unsigned freq)
+HeapNode *CreateHeapNode(char item, unsigned freq)
 {
-    HeapNode* temp = new HeapNode;
+    HeapNode *temp = new HeapNode;
     temp->left = temp->right = NULL;
     temp->letter = item;
     temp->frequency = freq;
@@ -45,31 +45,31 @@ HeapNode* CreateHeapNode(char item, unsigned freq)
 
 // Create min heap using given capacity
 // Returns a pointer to the min heap
-MinHeap* CreateMinHeap(unsigned capacity)
+MinHeap *CreateMinHeap(unsigned capacity)
 {
-    MinHeap* min_heap = new MinHeap;
+    MinHeap *min_heap = new MinHeap;
     min_heap->size = 0;
     min_heap->capacity = capacity;
-    min_heap->array = new HeapNode*[capacity];
+    min_heap->array = new HeapNode *[capacity];
     return min_heap;
 }
 
-string ConvertArrayOfIntegerToString(const vector<int>& arr)
+string ConvertArrayOfIntegerToString(const vector<int> &arr)
 {
     string output_string = "";
 
-    for (int i : arr)
+    for (size_t i = 0; i < arr.size(); i++)
     {
-        output_string += to_string(i);
+        output_string += to_string(arr[i]);
     }
 
     return output_string;
 }
 
 // Swap function
-void SwapHuffmanTreeNode(HeapNode** a, HeapNode** b)
+void SwapHuffmanTreeNode(HeapNode **a, HeapNode **b)
 {
-    HeapNode* temp = *a;
+    HeapNode *temp = *a;
     *a = *b;
     *b = temp;
 }
@@ -77,7 +77,7 @@ void SwapHuffmanTreeNode(HeapNode** a, HeapNode** b)
 // Re-arrange heap to make a min-heap.
 // If left is smaller than parent, swap parent and left
 // If right is smaller than parent, swap parent and right
-void MinHeapify(MinHeap* minHeap, int idx)
+void MinHeapify(MinHeap *minHeap, int idx)
 {
     int parent = idx;
     int left = 2 * idx + 1;
@@ -101,9 +101,9 @@ void MinHeapify(MinHeap* minHeap, int idx)
 // Remove the first node, replace it with the last node
 // Resize the heap i.e minus one
 // Re-arrange heap to make a min-heap.
-HeapNode* ExtractMinimumNode(MinHeap* min_heap)
+HeapNode *ExtractMinimumNode(MinHeap *min_heap)
 {
-    HeapNode* temp = min_heap->array[0];
+    HeapNode *temp = min_heap->array[0];
     min_heap->array[0] = min_heap->array[min_heap->size - 1];
 
     --min_heap->size;
@@ -113,7 +113,7 @@ HeapNode* ExtractMinimumNode(MinHeap* min_heap)
 }
 
 // Insertion
-void InsertMinHeap(MinHeap* min_heap, HeapNode* min_heap_node)
+void InsertMinHeap(MinHeap *min_heap, HeapNode *min_heap_node)
 {
     ++min_heap->size;
     int i = min_heap->size - 1;
@@ -127,11 +127,11 @@ void InsertMinHeap(MinHeap* min_heap, HeapNode* min_heap_node)
     min_heap->array[i] = min_heap_node;
 }
 
-MinHeap* CreateMinHeapAndHeapify(char item[], int freq[], int size)
+MinHeap *CreateMinHeapAndHeapify(char item[], int freq[], int size)
 {
-    MinHeap* new_min_heap = CreateMinHeap(size);
+    MinHeap *new_min_heap = CreateMinHeap(size);
 
-    for (int i = 0; i < size; ++i)
+    for (int i = 0; i < size; i++)
         new_min_heap->array[i] = CreateHeapNode(item[i], freq[i]);
 
     new_min_heap->size = size;
@@ -143,10 +143,10 @@ MinHeap* CreateMinHeapAndHeapify(char item[], int freq[], int size)
     return new_min_heap;
 }
 
-HeapNode* BuildHuffmanTree(char item[], int freq[], int size)
+HeapNode *BuildHuffmanTree(char item[], int freq[], int size)
 {
-    HeapNode* left, *right, *new_node;
-    MinHeap* min_heap = CreateMinHeapAndHeapify(item, freq, size);
+    HeapNode *left, *right, *new_node;
+    MinHeap *min_heap = CreateMinHeapAndHeapify(item, freq, size);
 
     while (min_heap->size != 1)
     {
@@ -166,7 +166,7 @@ HeapNode* BuildHuffmanTree(char item[], int freq[], int size)
 
 // For each non-leaf node, assign 0 to the left edge and 1 to the right edge.
 // When you encounter a leaf node, print all the already assigned numbers
-void AssignNumbersToEdges(HeapNode* root, vector<int>& code_array, int top, vector<TableData*>& table_data)
+void AssignNumbersToEdges(HeapNode *root, vector<int> &code_array, int top, vector<TableData *> &table_data)
 {
     if (root->left)
     {
@@ -189,22 +189,19 @@ void AssignNumbersToEdges(HeapNode* root, vector<int>& code_array, int top, vect
         table_data.push_back(new TableData{
             root,
             ConvertArrayOfIntegerToString(code_array),
-            top
-        });
+            top});
     }
 }
 
-
-
-void GenerateHuffmanCodes(char item[], int freq[], int size, vector<TableData*>& table_data)
+void GenerateHuffmanCodes(char item[], int freq[], int size, vector<TableData *> &table_data)
 {
-    HeapNode* root = BuildHuffmanTree(item, freq, size);
+    HeapNode *root = BuildHuffmanTree(item, freq, size);
 
     vector<int> code_array;
     AssignNumbersToEdges(root, code_array, 0, table_data);
 }
 
-void PrintTabularResult(vector<TableData*>& table_data)
+void PrintTabularResult(vector<TableData *> &table_data)
 {
     // Header
     FormatDataInTabularForm("Letter");
@@ -222,15 +219,20 @@ void PrintTabularResult(vector<TableData*>& table_data)
     cout << endl;
 
     // Body
-    for (const auto& entry : table_data)
+    for (size_t i = 0; i < table_data.size(); i++)
     {
-        FormatDataInTabularForm(entry->root->letter);
-        FormatDataInTabularForm(entry->root->frequency);
-        FormatDataInTabularForm(entry->code_array);
-        FormatDataInTabularForm(entry->code_array.length());
-        FormatDataInTabularForm(entry->code_array.length() * entry->root->frequency);
+        FormatDataInTabularForm(table_data[i]->root->letter);
+        FormatDataInTabularForm(table_data[i]->root->frequency);
+        FormatDataInTabularForm(table_data[i]->code_array);
+        FormatDataInTabularForm(table_data[i]->code_array.length());
+        FormatDataInTabularForm(table_data[i]->code_array.length() * table_data[i]->root->frequency);
         cout << endl;
     }
+}
+
+bool CompareTableData(TableData *a, TableData *b)
+{
+    return a->root->letter < b->root->letter;
 }
 
 int main()
@@ -238,28 +240,27 @@ int main()
     char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     int letters_frequencies[] = {77, 17, 32, 42, 120, 24, 17, 50, 76, 4, 7, 42, 24, 67, 67, 20, 5, 59, 67, 85, 37, 12, 22, 4, 22, 2};
 
-    vector<TableData*> table_data;
+    vector<TableData *> table_data;
     GenerateHuffmanCodes(letters, letters_frequencies, letters_size, table_data);
     // Sort data in alphabetical order.
-    sort(table_data.begin(), table_data.end(), [](TableData* a, TableData* b) {
-        return a->root->letter < b->root->letter;
-    });
+    sort(table_data.begin(), table_data.end(), CompareTableData);
+
     // Print
     PrintTabularResult(table_data);
 
     int sum_of_letters_frequencyxlenght = 0;
-    for (const auto& entry : table_data)
+    for (size_t i = 0; i < table_data.size(); i++)
     {
-        sum_of_letters_frequencyxlenght += (entry->code_array.length() * entry->root->frequency);
+        sum_of_letters_frequencyxlenght += (table_data[i]->code_array.length() * table_data[i]->root->frequency);
     }
 
     cout << endl;
     cout << "The weighted minimum path length is: " << sum_of_letters_frequencyxlenght << " bits" << endl;
 
     // memory clean up
-    for (auto entry : table_data)
+    for (size_t i = 0; i < table_data.size(); i++)
     {
-        delete entry;
+        delete table_data[i];
     }
 
     return 0;
